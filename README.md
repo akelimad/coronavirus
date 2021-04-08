c7d583157907c0d8663f5bda5bb0b9bd14685279
akelimad <akel.dev@gmail.com>
2021-04-08 15:26:31 +0000

    gitignore

--- README.md
+++
@@ -1,2 +0,0 @@
-# this is refont of coronavirus website 
-realized by Imad AKEL & Mohamed Amine Toufiki
--- assets/js/script.js
+++
@@ -1,3 +0,0 @@
-$('document').ready(function () {
-    $('#main_table_countries_today').DataTable();
-})
